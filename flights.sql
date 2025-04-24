245f9bd62415d387043e237b0c8ba154119696b4
Rajveer Kaur <rajveer.kaur@learningworks.co.nz>
2025-04-24 14:11:15 +1200

    add the terminal stuff how it lok like in the terminal

---
+++ flights.sql
@@ -0,0 +1,39 @@
+CREATE TABLE flights(
+   ...> id integer primary key autoincrement,
+   ...> origin TEXT NOT NULL,
+   ...> destination TEXT NOT NULL,
+   ...> duration INTEGER NOT NULL
+   ...> );
+
+insert into flights (origin,destination,duration)values("NZ","Australia",1400);
+
+sqlite> .TABLES
+Error: unknown command or invalid arguments:  "TABLES". Enter ".help" for help
+sqlite> .tables
+flights
+sqlite> select * from flights;
+sqlite> insert into flights (origin,destination,duration)values("NZ","Australia",1400);
+sqlite> select * from flights;
+1|NZ|Australia|1400
+sqlite> insert into flights (origin,destination,duration)values("New york","London",140);
+sqlite> insert into flights (origin,destination,duration)values("Lima","New York",435);
+sqlite> insert into flights (origin,destination,duration)values("Moscow","Paris",245);
+
+
+sqlite> select * from flights;
+1|NZ|Australia|1400
+2|New york|London|140
+3|Lima|New York|435
+4|Moscow|Paris|245
+
+
+
+sqlite> .mode columns
+sqlite> . headers yes
+sqlite> select * from flights;
+id  origin    destination  duration
+--  --------  -----------  --------
+1   NZ        Australia    1400    
+2   New york  London       140     
+3   Lima      New York     435     
+4   Moscow    Paris        245   
